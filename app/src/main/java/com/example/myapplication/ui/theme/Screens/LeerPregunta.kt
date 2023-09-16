74c47a017e3c54e61124e2e0b54d6bf2694282ab
HAROUN <haroundominique@gmail.com>
2023-09-16 21:42:03 +0200

    success implementing a reading questions and showing their answers. Next step: implement persistence probably through Json Files

--- app/src/main/java/com/example/myapplication/ui/theme/Screens/LeerPregunta.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Screens/LeerPregunta.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -26,15 +28,21 @@ import com.example.myapplication.ui.theme.ViewModel.JuliViewModel
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun LeerPregunta(vm : JuliViewModel, navController: NavController){
+fun LeerPregunta(vm: JuliViewModel, navController: NavController) {
 
     val questionEntries = vm.listOfQuestions;
+    val answerEntries = vm.listOfAnswers;
     var isBottomSheetVisible by remember { mutableStateOf(false) }
+    var questionChoosed = "";
+    var answerChoosed = "";
+    var index = 0;
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -43,18 +51,53 @@ fun LeerPregunta(vm : JuliViewModel, navController: NavController){
     ) {
 
         if (questionEntries.isNotEmpty()) {
+            var selectedQuestionIndex = -1;
             questionEntries.forEach { entry ->
+                selectedQuestionIndex++
+                index = selectedQuestionIndex;
                 Button(onClick = {
                     isBottomSheetVisible = true;
                     // Acción cuando se hace clic en el botón
                     // Por ejemplo, puedes mostrar el texto de la entrada en la consola
-                    println("LOG: Se hizo clic en el botón de pregunta")
+                    //guardo la pregunta y su respuesta en las variables que posteriormente se mostrarán en el bottomsheet:
+                    println("El índice de esta pregunta es: " + index)
+
+                    //println("LOG: Se hizo clic en el botón de pregunta")
                     //entry.get
                 }) {
                     Text(text = entry, style = TextStyle(fontSize = 20.sp))
                 }
             }
         }
+        /*
+                    if (isBottomSheetVisible) {
+                        ModalBottomSheetLayout(
+                            sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded),
+                            sheetContent = {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(200.dp)
+                                        .padding(16.dp)
+                                        .background(Color.Gray),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = "Contenido del Bottom Sheet")
+                                    Text(text = "PREGUNTA: "+questionEntries.get(index).toString())
+                                    Text(text = "RESPUESTA: "+answerEntries.get(index).toString())
+
+                                }
+                            }
+                        ) {
+                            // Contenido principal de la pantalla
+                            Text(text = "PREGUNTA: "+questionEntries.get(index).toString())
+                            Text(text = "RESPUESTA: "+answerEntries.get(index).toString())
+                        }
+                    }
+
+         */
+
+
         if (isBottomSheetVisible) {
             ModalBottomSheetLayout(
                 sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded),
@@ -62,16 +105,62 @@ fun LeerPregunta(vm : JuliViewModel, navController: NavController){
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(200.dp)
+                            .fillMaxHeight()
                             .padding(16.dp)
-                            .background(Color.Gray),
-                        contentAlignment = Alignment.Center
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color(0xFFFFFFFF),
+                                        Color(0xFFA8BFD8)
+                                    )
+                                )
+                            ),
                     ) {
-                        Text(text = "Contenido del Bottom Sheet")
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "PREGUNTA: ",
+                                style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold)
+                            )
+                            Text(
+                                text = questionEntries.get(index).toString(),
+                                style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Normal)
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp)) // Espaciador
+
+                            Text(
+                                text = "RESPUESTA: ",
+                                style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold)
+                            )
+                            Text(
+                                text = answerEntries.get(index).toString(),
+                                style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Normal)
+                            )
+                            Spacer(modifier = Modifier.height(16.dp)) // Espaciador
+                            Spacer(modifier = Modifier.height(16.dp)) // Espaciador
+                            Spacer(modifier = Modifier.height(16.dp)) // Espaciador
+                        }
                     }
                 }
             ) {
-                // Contenido principal de la pantalla
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Contenido principal de la pantalla
+                    Button(onClick = { navController.navigate(route = Screens.Boton2.route) }) {
+                        Text(
+                            text = "VOLVER A MIS PREGUNTAS"
+                        )
+                    }
+                }
             }
         }
 
